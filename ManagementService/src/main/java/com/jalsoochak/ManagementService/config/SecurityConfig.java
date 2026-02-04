6e2d9c46cfdf8393237364d57b9ea37248fb29e5
otonye77 <oamietubodie@gmail.com>
2026-02-04 07:07:38 +0100

    fixed bugs

--- ManagementService/src/main/java/com/jalsoochak/ManagementService/config/SecurityConfig.java
+++ ManagementService/src/main/java/com/jalsoochak/ManagementService/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "http://localhost:3001"
+                "http://localhost:3001",
+                "https://jalsoochak.beehyv.com"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
